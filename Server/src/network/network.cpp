3e6fe8ca1819b9480afb458f9ff0b98c9bf82553
Fradet <axel.fradet@epitech.eu>
2024-09-19 13:18:33 +0200

    refacto: ajusting format of function

--- Server/src/network/network.cpp
+++ Server/src/network/network.cpp
@@ -13,17 +13,20 @@ Network::Server::Server(asio::io_context& io_context, int port)
     receive();
 }
 
-void Network::Server::setMessageHandler(MessageHandler callback) {
+void Network::Server::setMessageHandler(MessageHandler callback)
+{
     this->_messageHandler = std::move(callback);
 }
 
-void Network::Server::send_message(const std::string& message, const asio::ip::udp::endpoint& endpoint) {
+void Network::Server::send_message(const std::string& message, const asio::ip::udp::endpoint& endpoint)
+{
     _socket.async_send_to(
         asio::buffer(message), endpoint,
         [](const asio::error_code&, std::size_t) {});
 }
 
-void Network::Server::receive() {
+void Network::Server::receive()
+{
     _socket.async_receive_from(asio::buffer(_recv_buffer), this->_remote_endpoint,
         [this](const asio::error_code& error, std::size_t bytes_recvd) {
             if (!error && bytes_recvd > 0) {
@@ -33,6 +36,6 @@ void Network::Server::receive() {
                     this->_messageHandler(message, this->_remote_endpoint);
                 }
             }
-            receive();
+            this->receive();
     });
 }
