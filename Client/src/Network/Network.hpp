951ebdfe782528406634613a28388f7753bf99a0
Fradet <axel.fradet@epitech.eu>
2024-09-24 13:23:15 +0200

    feat: create namespace for client network

---
+++ Client/src/Network/Network.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type Client
+** File description:
+** Network
+*/
+
+#include <thread>
+#include <random>
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <asio.hpp>
+#include <stdexcept>
+#include <unordered_map>
+
+/**
+ * @namespace Network
+ * @brief Network-related classes and functions
+ */
+namespace Network {
+
+    /**
+     * @brief CLient class to Network (Connection to server, receive data and send data)
+     */
+    class Client;
+};
