25751dd482294c247a61f8fe60dd7c5e58774cdd
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-25 16:48:51 +0200

    feat: remove echo off from install files
    removed echo off from .bat, with small adjustement, using %PATH% instead of /home/tjerome-rocher/Desktop/flutter_linux_3.19.4-stable/flutter/bin:/home/tjerome-rocher/.rbenv/plugins/ruby-build/bin:/home/tjerome-rocher/.rbenv/shims:/home/tjerome-rocher/.rbenv/bin:/home/tjerome-rocher/.rbenv/shims:/home/tjerome-rocher/.rbenv/bin:/home/my_scripts:/home/tjerome-rocher/Desktop/flutter_linux_3.19.4-stable/flutter/bin:/home/tjerome-rocher/.cabal/bin:/home/tjerome-rocher/.ghcup/bin:/home/tjerome-rocher/.nvm/versions/node/v12.22.12/bin:/home/tjerome-rocher/.rbenv/plugins/ruby-build/bin:/home/tjerome-rocher/.rbenv/shims:/home/tjerome-rocher/.rbenv/bin:/home/tjerome-rocher/.rbenv/shims:/home/tjerome-rocher/.rbenv/bin:/home/my_scripts:/home/tjerome-rocher/.local/bin:/home/tjerome-rocher/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/var/lib/snapd/snap/bin:/home/tjerome-rocher/Desktop/tek3/R-Type/vcpkg:/home/tjerome-rocher/Desktop/tek3/R-Type/vcpkg for env vars update

--- install.bat
+++ install.bat
@@ -1,20 +1,19 @@
-@echo off
 winget install cmake
  
 curl https://aka.ms/vs/16/release/vs_buildtools.exe -o .\vs_buildtools.exe
 .\vs_buildtools.exe --quiet --wait --norestart --includeRecommended --add Microsoft.VisualStudio.Workload.VCTools
-cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\"
-setx PATH "$PATH;$(pwd)" /M
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
+setx PATH "%PATH%;$(pwd)" /M
+set PATH="%PATH%;${pwd}" /M
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build"
-vcvars32.bat amd64
-
+.\vcvars32.bat amd64
+ 
 curl https://github.com/microsoft/vcpkg/archive/refs/heads/master.zip -o vcpkg.zip
-tar vcpkg.zip 
+tar -xf vcpkg.zip
 cd vcpkg-master
-bootstrap-vcpkg.bat
-setx PATH "$PATH;$(pwd)" /M
-
+./bootstrap-vcpkg.bat
+setx PATH "%PATH%;$(pwd)" /M
+set PATH="%PATH%;${pwd}" /M
+ 
 cd ..
 vcpkg install
-
-@REM to start in an admin cmd
