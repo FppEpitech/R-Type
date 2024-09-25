8258fd085804e4dc9cfcdeee565fb8a405e3173b
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-25 17:32:18 +0200

    feat: add /M to setx install bat
    To make it affect the whole system

--- install.bat
+++ install.bat
@@ -1,10 +1,9 @@
 winget install cmake
- 
-curl https://aka.ms/vs/16/release/vs_buildtools.exe -o .\vs_buildtools.exe
-.\vs_buildtools.exe --quiet --wait --norestart --includeRecommended --add Microsoft.VisualStudio.Workload.VCTools
+
+curl https://aka.ms/vs/16/release/vs_buildtools.exe -o .\vs_buildtools.exe && .\vs_buildtools.exe --quiet --wait --norestart --includeRecommended --add Microsoft.VisualStudio.Workload.VCTools
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
-setx PATH "%PATH%;$(pwd)" /M
-set PATH="%PATH%;${pwd}" /M
+setx /M PATH "%PATH%;$(pwd)"
+set PATH="%PATH%;${pwd}"
 cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build"
 .\vcvars32.bat amd64
  
@@ -12,8 +11,8 @@ curl https://github.com/microsoft/vcpkg/archive/refs/heads/master.zip -o vcpkg.z
 tar -xf vcpkg.zip
 cd vcpkg-master
 ./bootstrap-vcpkg.bat
-setx PATH "%PATH%;$(pwd)" /M
-set PATH="%PATH%;${pwd}" /M
- 
+setx /M PATH "%PATH%;$(pwd)"
+set PATH="%PATH%;${pwd}"
+
 cd ..
 vcpkg install
