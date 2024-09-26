44d0ec51236145f1c8658d489ab8cc23e3ccdbcf
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-26 13:59:00 +0200

    feat: epitech headers for .sh and .bat install files
    Added the project's title and a light description to make it fit the whole project

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+##
+## EPITECH PROJECT, 2024
+## R-TYPE
+## File description:
+## Install compilers, cmake, and set up the env vars.
+##
+
 sudo dnf update
 
 sudo dnf install gcc gcc-c++
@@ -10,12 +17,12 @@ cd vcpkg-master
 
 ./bootstrap-vcpkg.sh
 
-if ! grep -q 'export PATH=.*vcpkg' ~/.bashrc; then
-    echo 'export PATH="$PATH:'$(pwd)'"' >> ~/.bashrc
-fi
+# if ! grep -q 'export PATH=.*vcpkg' ~/.bashrc; then
+#     echo 'export PATH="$PATH:'$(pwd)'"' >> ~/.bashrc
+# fi
 
-source ~/.bashrc
+# source ~/.bashrc
 
 cd ..
 
-vcpkg install
+./vcpkg/vcpkg install
