99a4a7a0411c8d2862bf5af7490c3f79beece807
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-26 17:56:15 +0200

    feat: build.sh script dynamically get pwd
    Used hard coded var before

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-export VCPKG_ROOT=/home/tjerome-rocher/Desktop/tek3/R-Type/vcpkg-master && cmake . -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -B build
+export VCPKG_ROOT=${PWD}/vcpkg-master && cmake . -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake -B build
