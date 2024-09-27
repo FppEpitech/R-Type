44f241e007f8eb07f682157130fbfa5886df962a
Théophile JR <theophilejeromerocher44@gmail.com>
2024-09-27 20:10:38 +0200

    refactor(Scripts): using .bat for windows
    After a few tests, looks like the best solution. Sh must be ran with git bash on windows, and powershell is turned off by default on windows

--- tools/build-linux.sh
+++ tools/build-linux.sh
@@ -4,19 +4,13 @@
 ## EPITECH PROJECT, 2024
 ## R-TYPE
 ## File description:
-## Builds linux & windows r-type project.
+## Builds linux r-type project.
 ##
 
 VCPKG_ROOT="${PWD}/vcpkg"
 
-
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "mingw"* ]]; then
-    del /q *.exe
-    rmdir /s /q build
-else
-    rm -f *.exe
-    rm -rf build/
-fi
+rm -f *.exe
+rm -rf build/
 
 mkdir build
 
---
+++ tools/install-windows.bat
@@ -0,0 +1,28 @@
+@REM ##
+@REM ## EPITECH PROJECT, 2024
+@REM ## R-TYPE
+@REM ## File description:
+@REM ## Installs compilers, cmake, and set up the env vars.
+@REM ##
+
+winget install cmake
+
+wget --content-disposition --no-check-certificate https://aka.ms/vs/16/release/vs_buildtools.exe
+.\vs_buildtools.exe --quiet --wait --norestart --includeRecommended --add Microsoft.VisualStudio.Workload.VCTools
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
+@REM # setx /M PATH "%PATH%;$(pwd)"
+@REM # set PATH="%PATH%;${pwd}"
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build"
+.\vcvars32.bat amd64
+
+
+cd -
+
+git clone https://github.com/microsoft/vcpkg.git
+
+cd vcpkg
+
+./bootstrap-vcpkg.bat
+
+cd ..
+.\vcpkg\vcpkg install
