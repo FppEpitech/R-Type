cd12fe7d075c78c7d093e00b46d71490469d94c9
thomaltarix <thomas.boue@epitech.eu>
2024-09-27 17:29:25 +0200

    fix(DLLoader): Fix windows compilation from wrong lib
    I fixed the windows compilation which was due to a library include which had to be removed

--- include/DLLoader/DLLoader.hpp
+++ include/DLLoader/DLLoader.hpp
@@ -8,9 +8,9 @@
 #pragma once
 
 #ifdef _WIN32
-    #include <windows.h>
+#include <windows.h>
 #else
-    #include <dlfcn.h>
+#include <dlfcn.h>
 #endif
 
 #include <functional>
@@ -32,54 +32,53 @@ class DLLoader {
      */
     typedef T* (*CreateComponentFunc)();
 
-    public:
+public:
 
-        /**
-         * @brief Load a .so file and get a symbol function.
-         *
-         * @param libSo Path of the file to load.
-         * @param symbol Symbol of the function to call.
-         * @return std::shared_ptr<T> Return value of the function symbol.
-         */
-        static std::shared_ptr<T> load(const std::string& lib, const std::string& symbol) {
-
-            #ifdef _WIN32
-                std::string libName = lib;
+    /**
+     * @brief Load a .so file and get a symbol function.
+     *
+     * @param libSo Path of the file to load.
+     * @param symbol Symbol of the function to call.
+     * @return std::shared_ptr<T> Return value of the function symbol.
+     */
+    static std::shared_ptr<T> load(const std::string& lib, const std::string& symbol) {
+#ifdef _WIN32
+        std::string libName = lib;
                 size_t pos = libName.find(".dll");
                 if (pos != std::string::npos)
                     libName.replace(pos, 4, ".so");
-                void* handle = LoadLibrary(libName);
+                void* handle = LoadLibrary(libName.c_str());
                 if (!handle) {
                     std::cerr << "Failed to load library: " << libName << "on Windows" << std::endl;
                     return nullptr;
                 }
-                CreateComponentFunc createComponent = (CreateComponentFunc) GetProcAddress(static_cast<HMODULE>handle, symbol.c_str());
+                CreateComponentFunc createComponent = (CreateComponentFunc) GetProcAddress(static_cast<HMODULE>(handle), symbol.c_str());
 
                 if (!createComponent) {
                     std::cerr << "Cannot load symbol " << symbol << std::endl;
-                    FreeLibrary(static_cast<HMODULE>handle);
+                    FreeLibrary(static_cast<HMODULE>(handle));
                     return nullptr;
                 }
                 return std::shared_ptr<T>(createComponent());
-            #else
-                std::string libName = lib;
-                size_t pos = libName.find(".dll");
-                if (pos != std::string::npos)
-                    libName.replace(pos, 4, ".so");
-                void* handle = dlopen(libName.c_str(), RTLD_LAZY);
-                if (!handle) {
-                    std::cerr << "Failed to load library: " << libName << "on Linux" << std::endl;
-                    return nullptr;
-                }
-                CreateComponentFunc createComponent = (CreateComponentFunc) dlsym(handle, symbol.c_str());
-
-                const char* dlsym_error = dlerror();
-                if (dlsym_error) {
-                    std::cerr << "Cannot load symbol " << symbol << ": " << dlsym_error << std::endl;
-                    dlclose(handle);
-                    return nullptr;
-                }
-                return std::shared_ptr<T>(createComponent());
-            #endif
+#else
+        std::string libName = lib;
+        size_t pos = libName.find(".dll");
+        if (pos != std::string::npos)
+            libName.replace(pos, 4, ".so");
+        void* handle = dlopen(libName.c_str(), RTLD_LAZY);
+        if (!handle) {
+            std::cerr << "Failed to load library: " << libName << "on Linux" << std::endl;
+            return nullptr;
         }
+        CreateComponentFunc createComponent = (CreateComponentFunc) dlsym(handle, symbol.c_str());
+
+        const char* dlsym_error = dlerror();
+        if (dlsym_error) {
+            std::cerr << "Cannot load symbol " << symbol << ": " << dlsym_error << std::endl;
+            dlclose(handle);
+            return nullptr;
+        }
+        return std::shared_ptr<T>(createComponent());
+#endif
+    }
 };
