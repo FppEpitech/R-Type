cbe1a0bf07431e22dfc8c04dbc0d7188779857fc
thomaltarix <thomas.boue@epitech.eu>
2024-09-28 22:50:37 +0200

    feat(Scene Manager): Add an abstract class for the scene manager
    I add an abstract for the scene manager that handle the scene system, without processing inputs for now

---
+++ SceneManager/ASceneManager.hpp
@@ -0,0 +1,123 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** ASceneManager.hpp
+*/
+
+#pragma once
+
+#include <json/json.h>
+#include <unordered_map>
+
+#include "ISystem.hpp"
+#include "StringKeyMap.hpp"
+#include "ISceneManager.hpp"
+
+#define CONFIG_SUFFIX ".json"
+
+#ifdef _WIN32
+    #define LIB_SUFFIX ".dll"
+#else
+    #define LIB_SUFFIX ".so"
+#endif
+
+/**
+ * @brief Namespace for the scene manager.
+ * This namespace contains all the classes or enums that are related to the scene management.
+ */
+namespace SceneManager {
+
+    /**
+     * @brief Abstract class for the scene manager.
+     */
+    class ASceneManager : public ISceneManager {
+        public:
+
+            /**
+             * @brief Construct a new ASceneManager object.
+             * @param registries Registries for each scene.
+             */
+            ASceneManager(std::shared_ptr<std::vector<ECS::Registry>> registries);
+
+            /**
+             * @brief Destroy the ASceneManager object.
+             */
+            virtual ~ASceneManager() = default;
+
+            /**
+             * @brief Process the input of the scene.
+             * @param key Key to process.
+             * @return true if the input has been processed, false otherwise.
+             */
+            virtual bool processInput(KEY_MAP key) = 0;
+
+        protected:
+
+            std::shared_ptr<std::vector<ECS::Registry>> _registries; // Registries for each scene.
+            std::vector<std::unordered_map<KEY_MAP, std::shared_ptr<ISystem>>> _keysSystems; // Keys to load a system for each scene.
+            std::vector<std::unordered_map<KEY_MAP, std::pair<std::size_t, std::string>>> _keysScenes; // Keys to load a scene for each scene.
+
+            std::size_t _nextIndex; // Index of the next empty registry.
+
+            /**
+             * @brief Get the component lib path.
+             * @return std::string Path to the component lib.
+             */
+            virtual std::string _getComponentLibPath() const = 0;
+
+            /**
+             * @brief Get the system lib path.
+             * @return std::string Path to the system lib.
+             */
+            virtual std::string _getSystemLibPath() const = 0;
+
+            /**
+             * @brief Get the scenes path.
+             * @return std::string Path to the scenes.
+             */
+            virtual std::string _getScenesPath() const = 0;
+
+            /**
+             * @brief Load a scene from a json file.
+             * @param path Path to the json file.
+             * @param index Index of the registry to load the scene.
+             */
+            void _loadScene(const std::string &path, std::size_t index);
+
+            /**
+             * @brief Load the next scenes of a already loaded scene.
+             * @param path Path to the json file.
+             * @param index Index of the registry to load the scene.
+             */
+            void _loadNextScenes(const std::string &path, std::size_t index);
+
+            /**
+             * @brief Load the components of a scene.
+             * @param root Json root of the scene.
+             * @param index Index of the registry to load the scene.
+             */
+            void _loadSceneComponents(Json::Value root, std::size_t index);
+
+            /**
+             * @brief Load the systems of a scene.
+             * @param root Json root of the scene.
+             * @param index Index of the registry to load the scene.
+             */
+            void _loadSceneSystems(Json::Value root, std::size_t index);
+
+            /**
+             * @brief Load the keys of a scene.
+             * @param root Json root of the scene.
+             * @param index Index of the registry to load the scene.
+             */
+            void _loadSceneKeys(Json::Value root, std::size_t index);
+
+            /**
+             * @brief Change the current scene.
+             * @param scene Scene to load.
+             * @param key Key to load the scene.
+             */
+            void _changeScene(std::pair<std::size_t, std::string> scene);
+    };
+}
