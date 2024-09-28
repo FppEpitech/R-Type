8ba133ea75f17d1411fbe316fed3bf264c06bbcc
PerAlban <alban.peralta@epitech.eu>
2024-09-29 00:34:53 +0200

    feat(component): Create now component for colour
    Create a component to save the colour, with unsigned char to represent the rgba value between 0 and 255

---
+++ Client/src/Component/ColourComponent.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** ColourComponent.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "ColourComponent.hpp"
+
+ColourComponent::ColourComponent(int r, int g, int b, int a) :
+    AComponent("ColourComponent"), r(r), g(g), b(b), a(a) {}
