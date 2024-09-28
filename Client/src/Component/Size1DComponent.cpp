4037c33c4ee10f2246c9934a9206e0e4142102b1
PerAlban <alban.peralta@epitech.eu>
2024-09-28 21:04:03 +0200

    feat(component): Create component
    Create component that can be save a size of 1 dimension

---
+++ Client/src/Component/Size1DComponent.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** Size1DComponent.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "Size1DComponent.hpp"
+
+Size1DComponent::Size1DComponent(int size) :
+    AComponent("Size1DComponent"), size(size) {}
