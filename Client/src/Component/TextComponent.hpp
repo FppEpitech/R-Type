e1d211aaae1eef6dcb3fcce9e5653d6465b018fb
PerAlban <alban.peralta@epitech.eu>
2024-09-28 21:03:12 +0200

    feat(component): Create component
    Create component that can be save a text

---
+++ Client/src/Component/TextComponent.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2024
+** TextComponent.hpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "IComponent.hpp"
+
+/**
+ * @class TextComponent
+ * @brief Component for handling text in the graphical engine.
+ * @inherit AComponent (inherits IComponent)
+ *
+ * This component is used to store and manage text entities within the graphical engine.
+ *
+ * @var text The text content to be managed by this component.
+ */
+
+class TextComponent : public AComponent {
+    public:
+
+        std::string text; //< The text content.
+
+        /**
+         * @brief Construct a new Text Component object
+         *
+         * @param text Initial text content (default is an empty string).
+         */
+        TextComponent(std::string text = "");
+};
