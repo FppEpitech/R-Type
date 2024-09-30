3738798b17472d016b36038b87074bdecd816d35
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-09-30 13:23:59 +0200

    docs(Graphic): add commentaries for graphical classes/methods
    I added commentaries GraphicLib.hpp and GraphicalLoad.hpp

--- Engine/Client/Src/GraphicalLoad/GraphicalLoad.hpp
+++ Engine/Client/Src/GraphicalLoad/GraphicalLoad.hpp
@@ -18,28 +18,51 @@
 
 #include <vector>
 
+/**
+ * @brief Get the Graphical Library object.
+ *
+ * @return std::shared_ptr<IGraphic>
+ */
 std::shared_ptr<IGraphic> getGraphicalLibrary();
 
+/**
+ * @brief This class aim to load graphical systems and components in a register.
+ *
+ */
 class loadGraphicalEcs {
 
     public:
 
+        /**
+         * @brief Construct a new load Graphical Ecs object.
+         *
+         */
         loadGraphicalEcs() = default;
+
+        /**
+         * @brief Destroy the load Graphical Ecs object.
+         *
+         */
         ~loadGraphicalEcs() = default;
 
+        /**
+         * @brief Load all graphical systems and components int a register.
+         *
+         * @param registry Register to fill with systems ad components.
+         */
         void loadInRegister(ECS::Registry registry);
 
     private:
 
-        std::vector<std::reference_wrapper<IComponent>> _components{
+        std::vector<std::reference_wrapper<IComponent>> _components {
             _fontComponent,
             _position2DComponent,
             _size1DComponent,
             _textComponent
-        };
+        }; //< List of graphical components.
 
-        FontComponent           _fontComponent;
-        Position2DComponent     _position2DComponent;
-        Size1DComponent         _size1DComponent;
-        TextComponent           _textComponent;
+        FontComponent           _fontComponent;         //< Font component.
+        Position2DComponent     _position2DComponent;   //< Position 2D component.
+        Size1DComponent         _size1DComponent;       //< Size 1D component.
+        TextComponent           _textComponent;         //< Text component.
 };
--- GraphicLibrary/GraphicLib.hpp
+++ GraphicLibrary/GraphicLib.hpp
@@ -12,13 +12,46 @@
 
 #include <iostream>
 
+/**
+ * @brief Raylib graphical library.
+ *  This library will be compile into a .so file
+ *  to be use in our gameEngine.
+ *
+ */
 class GraphicLib : public IGraphic {
 
     public:
 
+        /**
+         * @brief Construct a new Graphic Lib object.
+         *
+         */
         GraphicLib() = default;
+
+        /**
+         * @brief Destroy the Graphic Lib object.
+         *
+         */
         ~GraphicLib();
+
+        /**
+         * @brief Init the graphic library/window.
+         *
+         * @param windowName Name of the window.
+         */
         void Init(const std::string &windowName);
+
+        /**
+         * @brief Check if the window is open.
+         *
+         * @return true The window is open.
+         * @return false The window is closed.
+         */
         bool windowIsOpen();
+
+        /**
+         * @brief Clear the window.
+         *
+         */
         void clear();
 };
