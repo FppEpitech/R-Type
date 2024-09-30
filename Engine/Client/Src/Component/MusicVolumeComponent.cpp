371903427eeb5f216a8c7080862229bb205a7f8b
PerAlban <alban.peralta@epitech.eu>
2024-09-30 13:30:54 +0200

    feat(client/component): Create component to handle music volume
    To store them in a registry, the music volume is defined by a float.

---
+++ Engine/Client/Src/Component/MusicVolumeComponent.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** MusicVolumeComponent.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "MusicVolumeComponent.hpp"
+
+MusicVolumeComponent::MusicVolumeComponent(float Volume)
+    : AComponent("MusicVolumeComponent"), Volume(Volume) {}
