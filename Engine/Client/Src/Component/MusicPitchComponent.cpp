03d68c3e3156c49da63cf9a177db772db998d169
PerAlban <alban.peralta@epitech.eu>
2024-09-30 13:38:53 +0200

    feat(client/components): Create component to handle the music pitch
    The pitch, is the speed or acceleration of the music

---
+++ Engine/Client/Src/Component/MusicPitchComponent.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** MusicPitchComponent.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "MusicPitchComponent.hpp"
+
+MusicPitchComponent::MusicPitchComponent(float Pitch)
+    : AComponent("MusicPitchComponent"), Pitch(Pitch) {}
