0899c63cde7876414091e0d4e2a59c9d0fd7fdc0
PerAlban <alban.peralta@epitech.eu>
2024-09-30 13:20:49 +0200

    feat(client/component): Create component to handle music path
    To store them in a registry

---
+++ Engine/Client/Src/Component/MusicPathComponent.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** MusicPathComponent.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "MusicPathComponent.hpp"
+
+MusicPathComponent::MusicPathComponent(std::string Path)
+    : AComponent("MusicPathComponent"), Path(Path) {}
