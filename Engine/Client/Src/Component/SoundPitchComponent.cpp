06870a17c8c7e752d56f58eeea6aeae9b2a69498
PerAlban <alban.peralta@epitech.eu>
2024-09-30 13:42:15 +0200

    feat(client/components): Create component to handle the sound pitch
    The pitch is the speed or acceleration of the sound

---
+++ Engine/Client/Src/Component/SoundPitchComponent.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** SoundPitchComponent.cpp
+** File description:
+** DESCRIPTION
+*/
+
+#include "SoundPitchComponent.hpp"
+
+SoundPitchComponent::SoundPitchComponent(float Pitch)
+    : AComponent("SoundPitchComponent"), Pitch(Pitch) {}
