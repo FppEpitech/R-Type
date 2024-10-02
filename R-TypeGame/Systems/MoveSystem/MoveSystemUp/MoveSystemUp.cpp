c47e6d7d7a7a20cf550f2870e67c9887c06da547
Fradet <axel.fradet@epitech.eu>
2024-10-02 09:52:45 +0200

    fix: add a catch
    I've add acatch to handle error

--- R-TypeGame/Systems/MoveSystem/MoveSystemUp/MoveSystemUp.cpp
+++ R-TypeGame/Systems/MoveSystem/MoveSystemUp/MoveSystemUp.cpp
@@ -22,18 +22,22 @@ std::function<void(ECS::Registry& reg, int idxPacketEntities)> MoveSystemUp::get
 void MoveSystemUp::updateUpPosition(ECS::Registry& entityManager, int idxPacketEntities)
 {
     std::cout << "HERE 1" << std::endl;
-    ECS::SparseArray<IComponent> PositionComponentArray = entityManager.get_components<IComponent>("Position2DComponent");
-    std::cout << "HERE 2" << std::endl;
-    ECS::SparseArray<IComponent> SpeedComponentArray = entityManager.get_components<IComponent>("SpeedComponent");
-    std::cout << "HERE 3" << std::endl;
+    try {
+        ECS::SparseArray<IComponent> PositionComponentArray = entityManager.get_components<IComponent>("Position2DComponent");
+        std::cout << "HERE 2" << std::endl;
+        ECS::SparseArray<IComponent> SpeedComponentArray = entityManager.get_components<IComponent>("SpeedComponent");
+        std::cout << "HERE 3" << std::endl;
 
-    Position2DComponent* position = dynamic_cast<Position2DComponent*>(PositionComponentArray[idxPacketEntities].get());
-    std::cout << "HERE 4" << std::endl;
-    SpeedComponent* speed = dynamic_cast<SpeedComponent*>(SpeedComponentArray[idxPacketEntities].get());
-    std::cout << "HERE 5" << std::endl;
+        Position2DComponent* position = dynamic_cast<Position2DComponent*>(PositionComponentArray[idxPacketEntities].get());
+        std::cout << "HERE 4" << std::endl;
+        SpeedComponent* speed = dynamic_cast<SpeedComponent*>(SpeedComponentArray[idxPacketEntities].get());
+        std::cout << "HERE 5" << std::endl;
 
-    position->y -= speed->speedY;
-    std::cout << "After position->y: " << position->y << std::endl;
+        position->y -= speed->speedY;
+        std::cout << "After position->y: " << position->y << std::endl;
+    } catch(const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 }
 
 extern "C" ISystem* loadSystemInstance()
