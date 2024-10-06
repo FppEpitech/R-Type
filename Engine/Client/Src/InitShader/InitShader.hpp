a878d87ac054e54b5438dca3bc06c26ccd825396
PerAlban <alban.peralta@epitech.eu>
2024-10-06 17:46:27 +0200

    feat(Client): Create function to InitShader system
    Those functions, will get our settings.json in our game file
    after it called the gamelib function to init shader

---
+++ Engine/Client/Src/InitShader/InitShader.hpp
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2024
+** InitShader.hpp
+** File description:
+** DESCRIPTION
+*/
+
+#pragma once
+
+
+#include <iostream>
+#include <memory>
+
+#include "IGraphic.hpp"
+#include "IError.hpp"
+
+/**
+ * @brief System to draw a Shader.
+ *
+ * The Shader is a program that runs on the GPU.
+ * It link with a .fs file.
+ * In our game one example of shader is the system to adapt color for the colorblind.
+ */
+class InitShader {
+
+public:
+
+    class JsonError : public AError {
+        public:
+            JsonError(const std::string &message) : AError(message) {}
+    };
+
+    /**
+     * @brief Construct a new Draw Shader System object.
+     *
+     */
+    InitShader(std::shared_ptr<IGraphic> graphicLib, std::string settingsPath = "./Config/Settings.json");
+
+    /**
+     * @brief Destroy the Draw Shader System object.
+     *
+     */
+    ~InitShader() = default;
+};
