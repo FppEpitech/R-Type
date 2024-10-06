3abf9709fdb8b8fbecfa9ecc137e86ba2661b0d3
thomaltarix <thomas.boue@epitech.eu>
2024-10-06 22:34:07 +0200

    feat(Systems): Add a system that handle clicks
    This systems handle clicks by running the callbacks of all entities that has a 2d position, a texture rect, a scale and a callback

---
+++ R-TypeGame/Systems/ClicksSystems/ClicksSystem.cpp
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** ClicksSystem.cpp
+*/
+
+#include <iostream>
+
+#include "KeyMap.hpp"
+#include "ClicksSystem.hpp"
+#include "ScaleComponent.hpp"
+#include "CallBackComponent.hpp"
+#include "Position2DComponent.hpp"
+#include "GetGraphicalLibrary.hpp"
+#include "TextureRectComponent.hpp"
+
+ClicksSystem::ClicksSystem() : ASystem("ClicksSystem") {}
+
+void ClicksSystem::_handleClicks(ECS::Registry &reg, int idxPacketEntities)
+{
+    try {
+        std::shared_ptr<IGraphic> lib = getGraphicalLibrary();
+
+        if (!lib)
+            return;
+        std::pair<int, int> mousePos = lib->getMousePosition();
+
+        ECS::SparseArray<IComponent> positions = reg.get_components<IComponent>("Position2DComponent");
+        ECS::SparseArray<IComponent> texturesRect = reg.get_components<IComponent>("TextureRectComponent");
+        ECS::SparseArray<IComponent> callbacks = reg.get_components<IComponent>("CallBackComponent");
+        ECS::SparseArray<IComponent> scales = reg.get_components<IComponent>("ScaleComponent");
+
+        for (ECS::entity_t entity = 0; positions.size() >= entity + 1 && texturesRect.size() >= entity + 1 && callbacks.size() >= entity + 1 && scales.size() >= entity + 1; entity++) {
+            std::shared_ptr<Position2DComponent> position = std::dynamic_pointer_cast<Position2DComponent>(positions[entity]);
+            std::shared_ptr<TextureRectComponent> textureRect = std::dynamic_pointer_cast<TextureRectComponent>(texturesRect[entity]);
+            std::shared_ptr<CallBackComponent> callback = std::dynamic_pointer_cast<CallBackComponent>(callbacks[entity]);
+            std::shared_ptr<ScaleComponent> scale = std::dynamic_pointer_cast<ScaleComponent>(scales[entity]);
+            if (!position || !textureRect || !callback || !scale)
+                continue;
+            if (lib->isMouseButtonPressed(IGraphic::MouseButtons::MOUSE_LEFT) && mousePos.first >= position->x && mousePos.first <= position->x + textureRect->width * scale->scale && mousePos.second >= position->y && mousePos.second <= position->y + textureRect->height * scale->scale)
+                callback->callback(reg, idxPacketEntities);
+        }
+    } catch (std::exception e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+}
+
+extern "C" ISystem *loadSystemInstance()
+{
+    return new ClicksSystem();
+}
---
+++ R-TypeGame/Systems/ClicksSystems/ClicksSystem.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** ClicksSystem.hpp
+*/
+
+#pragma once
+
+#include "ISystem.hpp"
+
+/**
+ * @brief System to handle clicks.
+ */
+class ClicksSystem : public ASystem {
+
+    public:
+
+        /**
+         * @brief Construct a new Clicks System object.
+         *
+         */
+        ClicksSystem();
+
+        /**
+         * @brief Destroy the Clicks System object.
+         *
+         */
+        ~ClicksSystem() = default;
+
+        /**
+         * @brief Get the Function object.
+         *
+         * @return std::function<void(ECS::Registry& reg, int idxPacketEntities)>
+         */
+        std::function<void(ECS::Registry& reg, int idxPacketEntities)> getFunction()
+        {
+            return [this](ECS::Registry& reg, int idxPacketEntities) {
+                _handleClicks(reg, idxPacketEntities);
+            };
+        }
+
+    private:
+
+        void _handleClicks(ECS::Registry& reg, int idxPacketEntities); //< Function to handle clicks.
+};
