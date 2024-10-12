3a1aaff70d4e089919c3864408bf6418d42f05a1
thomaltarix <thomas.boue@epitech.eu>
2024-10-12 16:52:41 +0200

    feat(Game Data): Add the end scene system and config
    The end scene if composed of our basic background and our studio's logo

---
+++ R-TypeGame/Systems/Background/EndLogoInitSystem.hpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2024
+** EndLogoInitSystem
+** File description:
+** EndLogoInitSystem
+*/
+
+#pragma once
+
+#include <iostream>
+
+#include "ISystem.hpp"
+
+/**
+ * @brief Initialize an end logo entity.
+ *
+ */
+class EndLogoInitSystem : public ASystem {
+
+public:
+
+    /**
+     * @brief Construct a new end logo Init System object.
+     *
+     */
+    EndLogoInitSystem();
+
+    /**
+     * @brief Destroy the end logo Init System object.
+     *
+     */
+    ~EndLogoInitSystem() = default;
+
+    /**
+     * @brief Get the Function object.
+     *
+     * @return std::function<void(ECS::Registry& reg, int idxPacketEntities)>
+     */
+    std::function<void(ECS::Registry& reg, int idxPacketEntities)> getFunction()
+    {
+        return [this](ECS::Registry& reg, int idxPacketEntities) {
+            _initEndLogo(reg, idxPacketEntities);
+        };
+    }
+
+private:
+
+    void _initEndLogo(ECS::Registry& reg, int idxPacketEntities); //< Function to init the end logo.
+};
