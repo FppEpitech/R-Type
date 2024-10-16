f6cc49d5576cf4289f2c9330d903fb983fb1582d
Fradet <axel.fradet@epitech.eu>
2024-10-16 11:17:38 +0200

    feat(ABI Network): Add files and CMake
    I've added the file for ABI and Cmake to compile Lib

---
+++ Network/ABINetwork.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type Server
+** File description:
+** NetworkServer
+*/
+
+#include <thread>
+#include <random>
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <asio.hpp>
+#include <stdexcept>
+#include <unordered_map>
+
+
