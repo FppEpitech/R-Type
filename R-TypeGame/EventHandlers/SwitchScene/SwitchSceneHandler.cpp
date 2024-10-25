c6af7e9627612c23858105896d98a199c9f677c5
thomaltarix <thomas.boue@epitech.eu>
2024-10-25 13:25:35 +0200

    feat(EventHandlers): Add a event handler for the scene switches
    A button can now sent an event with the name of the scene as parameter and change scene

---
+++ R-TypeGame/EventHandlers/SwitchScene/SwitchSceneHandler.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** SwitchSceneHandler.cpp
+*/
+
+#include "AEvent.hpp"
+#include "SwitchSceneHandler.hpp"
+#include <iostream>
+
+SwitchSceneHandler::SwitchSceneHandler(std::string eventType)
+    : AEventHandler("SwitchScene") {}
+
+bool SwitchSceneHandler::processEvent(std::shared_ptr<IEvent> event,
+                                      std::shared_ptr<SceneManager::ISceneManager> sceneManager,
+                                      std::shared_ptr<ABINetwork::INetworkUnit> networkUnit,
+                                      std::shared_ptr<IGraphic> graphicLib)
+{
+    try {
+        std::string sceneName = std::any_cast<std::string>(event->getValues()[0]);
+        std::pair<std::size_t, std::string> scenePair = std::make_pair(0, sceneName);
+        sceneManager->changeScene(scenePair);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return false;
+    }
+    return true;
+}
+
+extern "C" {
+    EXPORT_SYMBOL IEventHandler *loadEventHandlerInstance() {
+        return new SwitchSceneHandler();
+    }
+}
