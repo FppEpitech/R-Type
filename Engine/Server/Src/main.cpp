eda9a67ddcef3b1f6aa1c3bee524439c23d85d63
Théophile JR <theophilejeromerocher44@gmail.com>
2024-10-25 19:31:31 +0200

    feat(Server): put back the original main after database dev
    Had to make changes to the main, putting of the server

--- Engine/Server/Src/main.cpp
+++ Engine/Server/Src/main.cpp
@@ -12,34 +12,11 @@
 
 int main()
 {
-    // try {
-    //     GameEngine::Application myApp;
-    //     myApp.run();
-    // } catch (const std::exception& e) {
-    //     std::cerr << "Exception: " << e.what() << std::endl;
-    // }
-
-    Database db("db.db");
-    Scores scores(&db);
-    Users users(&db);
-
-    std::cout << users.registerUser("theophile", "test") << std::endl;
-
-    std::cout << users.loginUser("theophile", "test"  ) << std::endl;
-
-
-    scores.addScore(1, 100);
-    for (auto i : scores.getLeaderboard(users)) {
-        std::cout << i.first << " " << i.second << std::endl;
+    try {
+        GameEngine::Application myApp;
+        myApp.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
-
-
-    // db.setUserSettings(1, 1920, 1080, 0.5f);
-
-    // auto settings = db.getUserSettings(1);
-    // std::cout << settings.daltonian_mode << std::endl;
-    // std::cout << settings.res_height << std::endl;
-    // std::cout << settings.res_width << std::endl;
-
     return 0;
 }
