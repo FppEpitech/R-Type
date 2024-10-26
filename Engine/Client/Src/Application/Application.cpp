25feaf4687b8250c2385771dafabbdb11685c365
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-10-26 17:39:57 +0200

    fix(Client App): Client run offline at the beginning
    I fix the way that the client didn't try to connect to the server at the beginning of the app

--- Engine/Client/Src/Application/Application.cpp
+++ Engine/Client/Src/Application/Application.cpp
@@ -29,85 +29,8 @@ Application::Application()
     _client = nullptr;
 }
 
-// void Application::run()
-// {
-//     InitWindow InitWindow(_libGraphic);
-//     InitShader InitShader(_libGraphic);
-//     ConsumptionCompute consumptionCompute;
-
-//     while (_libGraphic->windowIsOpen()) {
-//         _connectServer();
-//         _keyboardHandler(_libGraphic->getKeyDownInput());
-//         _libGraphic->startDraw();
-//         _libGraphic->clear();
-//         _registry->run_systems(-1);
-//         for (auto defaultSystem : _defaultSystems)
-//             defaultSystem(*_registry, -1);
-//         _eventListener->listen();
-//         _libGraphic->endDraw();
-//     }
-// }
-
-// void Application::_packetHandler(Network::UDPPacket packet, ECS::Registry& reg)
-// {
-//     uint32_t componentTypeLength = static_cast<size_t>(packet.getPayload()[0]);
-
-//     if (packet.getPayload().size() < 1 + componentTypeLength) {
-//         std::cerr << "Payload is too small." << std::endl;
-//         return;
-//     }
-//     std::string componentType(packet.getPayload().begin() + 1, packet.getPayload().begin() + 1 + componentTypeLength);
-
-//     int idxPacketEntities = (packet.getPayload()[1 + componentTypeLength] << 24) |
-//                             (packet.getPayload()[2 + componentTypeLength] << 16) |
-//                             (packet.getPayload()[3 + componentTypeLength] << 8)  |
-//                             packet.getPayload()[4 + componentTypeLength];
-
-//     // if (idxPacketEntities == this->_client->getIdxPlayerServer())
-//     //     return;
-
-//     _sceneManager->processUpdate(componentType, packet);
-// }
-
-// void Application::_connectServer()
-// {
-//     std::lock_guard<std::mutex> lock(this->_registry->_myBeautifulMutex);
-//     try {
-//         ECS::SparseArray<IComponent> players = this->_registry->get_components<IComponent>("PlayerComponent");
-//         ECS::SparseArray<IComponent> buttonNetworkConnection = this->_registry->get_components<IComponent>("NetworkConnectionComponent");
-
-//         for (int index = 0; index < buttonNetworkConnection.size(); index++) {
-//             std::shared_ptr<NetworkConnectionComponent> networkInfo = std::dynamic_pointer_cast<NetworkConnectionComponent>(buttonNetworkConnection[index]);
-//             if (!networkInfo)
-//                 continue;
-//             if (networkInfo->connect == true) {
-//                 networkInfo->connect = false;
-//                 _sceneManager->changeScene(std::make_pair<size_t, std::string>(0, "firstScene.json"));
-//                  _client = std::make_shared<Network::Client>(networkInfo->serverIp, std::atoi(networkInfo->serverPort.c_str()), 4445);
-//                 _client->connect([this](Network::UDPPacket packet, ECS::Registry& reg) {
-//                     this->_packetHandler(std::move(packet), *_registry);
-//                 }, *_registry);
-//             }
-//         }
-//         if (_client == nullptr)
-//             return;
-//         if (_client->getIdxPlayerComponent() != -1 && _client->getIdxPlayerComponent() < players.size()) {
-//             std::shared_ptr<PlayerComponent> player = std::dynamic_pointer_cast<PlayerComponent>(players[_client->getIdxPlayerComponent()]);
-//             if (player && !player->isAlive) {
-//                 player->isAlive = true;
-//                 _sceneManager->changeScene(std::make_pair<size_t, std::string>(0, "endScene.json"));
-//             }
-//         }
-//     } catch(const std::exception& e) {
-//         std::cerr << e.what() << std::endl;
-//     }
-// }
-
 void Application::run()
 {
-    // std::shared_ptr<IGraphic> libGraphic = getGraphicalLibrary();
-    // if (!libGraphic)
-    //     throw ClientError("Failed to load graphic library");
     InitWindow InitWindow(_libGraphic);
     InitShader InitShader(_libGraphic);
     ConsumptionCompute consumptionCompute;
@@ -121,8 +44,10 @@ void Application::run()
         _registry->run_systems(-1);
         for (auto defaultSystem : _defaultSystems)
             defaultSystem(*_registry, -1);
+        _eventListener->listen();
         _libGraphic->endDraw();
-        ABINetwork::sendMessages(_client);
+        if (_client)
+            ABINetwork::sendMessages(_client);
     }
 }
 
@@ -154,6 +79,8 @@ void Application::_keyboardHandler(std::size_t key)
 
 void Application::_packetHandler()
 {
+    if (!_client)
+        return;
     std::lock_guard<std::mutex> lock(_client->getMutex());
     std::vector<ABINetwork::UDPPacket> messages = _client->getReceivedMessages();
 
@@ -204,11 +131,11 @@ void Application::_handleFullRoomPacket(ABINetwork::UDPPacket packet)
 
 void Application::_connectServer()
 {
-    if (!_client) {
-        _client = ABINetwork::createClient("127.0.0.1", 4444, 4445);
-        _serverInfos.tcpPort = 4444;
-        _serverInfos.udpPort = 4445;
-    }
+    // if (!_client) {
+    //     _client = ABINetwork::createClient("127.0.0.1", 4444, 4445);
+    //     _serverInfos.tcpPort = 4444;
+    //     _serverInfos.udpPort = 4445;
+    // }
 
     // std::lock_guard<std::mutex> lock(this->_registry->_myBeautifulMutex);
     // try {
@@ -232,7 +159,6 @@ void Application::_connectServer()
     //         return;
     //     if (_client->getIdxPlayerComponent() != -1 && _client->getIdxPlayerComponent() < players.size()) {
     //         std::shared_ptr<PlayerComponent> player = std::dynamic_pointer_cast<PlayerComponent>(players[_client->getIdxPlayerComponent()]);
-
     //         if (player && !player->isAlive) {
     //             player->isAlive = true;
     //             _sceneManager->_changeScene(std::make_pair<size_t, std::string>(0, "endScene.json"));
