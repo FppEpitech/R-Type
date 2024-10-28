873bbf2084fc6600c4122c852dc634e44eb6cde0
thomaltarix <thomas.boue@epitech.eu>
2024-10-28 16:43:32 +0100

    feat(Default Components): Add the checkable component
    A entity that has this component can be checked and unchecked. It another entity with this component is checked, the first isn't unchecked

---
+++ Engine/Shared/DefaultComponents/Checkable/CheckableComponent.cpp
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** CheckableComponent.cpp
+*/
+
+#include "CheckableComponent.hpp"
+
+CheckableComponent::CheckableComponent(bool isChecked) : isChecked(isChecked), AComponent("CheckableComponent") {}
