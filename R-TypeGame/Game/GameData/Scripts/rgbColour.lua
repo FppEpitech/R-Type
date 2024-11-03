3c24e807045218448aff424bf35844cf7f14bf70
Théophile J-R <theophilejeromerocher44@gmail.com>
2024-11-03 01:15:24 +0100

    feat(LuaScript): Rgb activated on the main menu

--- R-TypeGame/Game/GameData/Scripts/rgbColour.lua
+++ R-TypeGame/Game/GameData/Scripts/rgbColour.lua
@@ -1,16 +1,16 @@
--- for i = 0, getComponentsSize("ColourComponent") - 1 do
---     local r = getComponentIntArg(i, "ColourComponent", "r")
---     local g = getComponentIntArg(i, "ColourComponent", "g")
---     local b = getComponentIntArg(i, "ColourComponent", "b")
---     local a = getComponentIntArg(i, "ColourComponent", "a")
---     if r ~= nil and g ~= nil and b ~= nil and a ~= nil then
---         setComponentIntArg(i, "ColourComponent", "r", r + 1)
---         setComponentIntArg(i, "ColourComponent", "g", g + 2)
---         setComponentIntArg(i, "ColourComponent", "b", b + 3)
---         -- setComponentIntArg(i, "ColourComponent", "a", info + 1)
---     end
+ for i = 0, getComponentsSize("ColourComponent") - 1 do
+     local r = getComponentIntArg(i, "ColourComponent", "r")
+     local g = getComponentIntArg(i, "ColourComponent", "g")
+     local b = getComponentIntArg(i, "ColourComponent", "b")
+     local a = getComponentIntArg(i, "ColourComponent", "a")
+     if r ~= nil and g ~= nil and b ~= nil and a ~= nil then
+         setComponentIntArg(i, "ColourComponent", "r", r + 1)
+         setComponentIntArg(i, "ColourComponent", "g", g + 2)
+         setComponentIntArg(i, "ColourComponent", "b", b + 3)
+          --setComponentIntArg(i, "ColourComponent", "a", info + 1)
+     end
 
--- end
+end
 
 -- rgb for shoot colour components
 -- for i = 0, getComponentsSize("ColourComponent") - 1 do
