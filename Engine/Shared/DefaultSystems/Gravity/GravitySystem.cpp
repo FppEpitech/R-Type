5eca7d084cb59d61d5f9d5888816708636d6ea08
thomaltarix <thomas.boue@epitech.eu>
2024-11-03 01:10:28 +0100

    feat(Gravity System): Apply movement of the hitboxes when gravity is applied
    When the gravity is applied, the entities are moving so basically, the hitboxes too, so I update them now

--- Engine/Shared/DefaultSystems/Gravity/GravitySystem.cpp
+++ Engine/Shared/DefaultSystems/Gravity/GravitySystem.cpp
@@ -7,7 +7,11 @@
 
 #include <iostream>
 
+#include "AEvent.hpp"
 #include "GravitySystem.hpp"
+#include "ScaleComponent.hpp"
+#include "HitBoxComponent.hpp"
+#include "ObjPathComponent.hpp"
 #include "GravityComponent.hpp"
 #include "Position3DComponent.hpp"
 
@@ -19,6 +23,9 @@ void GravitySystem::_handleGravity(ECS::Registry &reg, int idxPacketEntities)
     try {
         ECS::SparseArray<IComponent> positions3D = reg.get_components<IComponent>("Position3DComponent");
         ECS::SparseArray<IComponent> gravities = reg.get_components<IComponent>("GravityComponent");
+        ECS::SparseArray<IComponent> hitBoxes = reg.get_components<IComponent>("HitBoxComponent");
+        ECS::SparseArray<IComponent> objPaths = reg.get_components<IComponent>("ObjPathComponent");
+        ECS::SparseArray<IComponent> scales = reg.get_components<IComponent>("ScaleComponent");
 
         for (ECS::entity_t entity = 0; entity < positions3D.size() && entity < gravities.size(); entity++) {
             std::shared_ptr<Position3DComponent> position3D = std::dynamic_pointer_cast<Position3DComponent>(positions3D[entity]);
@@ -30,6 +37,30 @@ void GravitySystem::_handleGravity(ECS::Registry &reg, int idxPacketEntities)
             position3D->x -= gravity->gravityX;
             position3D->y -= gravity->gravityY;
             position3D->z -= gravity->gravityZ;
+
+            std::vector<std::any> values = {};
+            values.push_back((float)position3D->x);
+            values.push_back((float)position3D->y);
+            values.push_back((float)position3D->z);
+            std::shared_ptr<IEvent> event = std::make_shared<AEvent>("UpdateCamera", values);
+            reg.addEvent(event);
+
+            if (entity < hitBoxes.size() && entity < objPaths.size() && entity < scales.size()) {
+                std::shared_ptr<HitBoxComponent> hitBox = std::dynamic_pointer_cast<HitBoxComponent>(hitBoxes[entity]);
+                std::shared_ptr<ObjPathComponent> objPath = std::dynamic_pointer_cast<ObjPathComponent>(objPaths[entity]);
+                std::shared_ptr<ScaleComponent> scale = std::dynamic_pointer_cast<ScaleComponent>(scales[entity]);
+                if (hitBox && objPath && scale) {
+                    std::vector<std::any> hitBoxValues = {};
+                    hitBoxValues.push_back((float)position3D->x);
+                    hitBoxValues.push_back((float)position3D->y);
+                    hitBoxValues.push_back((float)position3D->z);
+                    hitBoxValues.push_back((float)scale->scale);
+                    hitBoxValues.push_back((std::string)objPath->path);
+                    hitBoxValues.push_back((int)entity);
+                    std::shared_ptr<IEvent> hitBoxEvent = std::make_shared<AEvent>("UpdateHitBox", hitBoxValues);
+                    reg.addEvent(hitBoxEvent);
+                }
+            }
         }
     } catch (std::exception e) {
     }
