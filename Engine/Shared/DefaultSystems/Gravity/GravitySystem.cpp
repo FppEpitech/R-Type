de83c7a7df9ea76023f1f03346d2a6392cfb9eea
thomaltarix <thomas.boue@epitech.eu>
2024-11-03 14:26:20 +0100

    feat(Gravity System): Apply collision damage and jump component
    Now, the gravity system applies collision damage and modify the canJump component so that the player can jump (or not)

--- Engine/Shared/DefaultSystems/Gravity/GravitySystem.cpp
+++ Engine/Shared/DefaultSystems/Gravity/GravitySystem.cpp
@@ -9,11 +9,16 @@
 
 #include "AEvent.hpp"
 #include "GravitySystem.hpp"
+#include "LifeComponent.hpp"
+#include "DrawComponent.hpp"
 #include "ScaleComponent.hpp"
 #include "HitBoxComponent.hpp"
+#include "PlayerComponent.hpp"
+#include "CanJumpComponent.hpp"
 #include "ObjPathComponent.hpp"
 #include "GravityComponent.hpp"
 #include "Position3DComponent.hpp"
+#include "ColliderDamageComponent.hpp"
 
 GravitySystem::GravitySystem() : ASystem("GravitySystem") {}
 
@@ -37,15 +42,43 @@ static void updateHitBox(ECS::Registry &reg, ECS::entity_t entity, ECS::SparseAr
     }
 }
 
+static void applyCollisionDamage(ECS::Registry &reg, ECS::entity_t entity, ECS::entity_t entity2, ECS::SparseArray<IComponent> &colliderDamages, ECS::SparseArray<IComponent> &lives, ECS::SparseArray<IComponent> &draws)
+{
+    for (int i = 0; i < 2; ++i) {
+        ECS::entity_t e1 = (i == 0) ? entity : entity2;
+        ECS::entity_t e2 = (i == 0) ? entity2 : entity;
+        if (e1 < colliderDamages.size() && e2 < lives.size()) {
+            std::shared_ptr<ColliderDamageComponent> colliderDamage = std::dynamic_pointer_cast<ColliderDamageComponent>(colliderDamages[e1]);
+            std::shared_ptr<LifeComponent> life = std::dynamic_pointer_cast<LifeComponent>(lives[e2]);
+            if (!colliderDamage || !life)
+                continue;
+            if (life->life <= colliderDamage->damage) {
+                life->life = 0;
+                if (e2 < draws.size()) {
+                    std::shared_ptr<DrawComponent> draw = std::dynamic_pointer_cast<DrawComponent>(draws[e2]);
+                    if (draw)
+                        draw->draw = false;
+                }
+            } else
+                life->life -= colliderDamage->damage;
+        }
+    }
+}
+
 void GravitySystem::_handleGravity(ECS::Registry &reg, int idxPacketEntities)
 {
     std::lock_guard<std::mutex> lock(reg._myBeautifulMutex);
     try {
+        ECS::SparseArray<IComponent> draws = reg.get_components<IComponent>("DrawComponent");
         ECS::SparseArray<IComponent> positions3D = reg.get_components<IComponent>("Position3DComponent");
         ECS::SparseArray<IComponent> gravities = reg.get_components<IComponent>("GravityComponent");
         ECS::SparseArray<IComponent> hitBoxes = reg.get_components<IComponent>("HitBoxComponent");
         ECS::SparseArray<IComponent> objPaths = reg.get_components<IComponent>("ObjPathComponent");
         ECS::SparseArray<IComponent> scales = reg.get_components<IComponent>("ScaleComponent");
+        ECS::SparseArray<IComponent> players = reg.get_components<IComponent>("PlayerComponent");
+        ECS::SparseArray<IComponent> colliderDamages = reg.get_components<IComponent>("ColliderDamageComponent");
+        ECS::SparseArray<IComponent> lives = reg.get_components<IComponent>("LifeComponent");
+        ECS::SparseArray<IComponent> canJumps = reg.get_components<IComponent>("CanJumpComponent");
 
         for (ECS::entity_t entity = 0; entity < positions3D.size() && entity < gravities.size(); entity++) {
             std::shared_ptr<Position3DComponent> position3D = std::dynamic_pointer_cast<Position3DComponent>(positions3D[entity]);
@@ -68,6 +101,7 @@ void GravitySystem::_handleGravity(ECS::Registry &reg, int idxPacketEntities)
                     continue;
                 if (hitBox->isColliding(hitBox2->hitBoxes)) {
                     isColliding = true;
+                    applyCollisionDamage(reg, entity, entity2, colliderDamages, lives, draws);
                     break;
                 }
             }
@@ -75,16 +109,32 @@ void GravitySystem::_handleGravity(ECS::Registry &reg, int idxPacketEntities)
                 position3D->x += gravity->gravityX;
                 position3D->y += gravity->gravityY;
                 position3D->z += gravity->gravityZ;
-                break;
+                if (entity < canJumps.size()) {
+                    std::shared_ptr<CanJumpComponent> canJump = std::dynamic_pointer_cast<CanJumpComponent>(canJumps[entity]);
+                    if (!canJump)
+                        continue;
+                    canJump->canJump = true;
+                }
             }
-
-            std::vector<std::any> values = {};
-            values.push_back((float)position3D->x);
-            values.push_back((float)position3D->y);
-            values.push_back((float)position3D->z);
-            std::shared_ptr<IEvent> event = std::make_shared<AEvent>("UpdateCamera", values);
-            reg.addEvent(event);
-
+            if (entity < players.size()) {
+                std::shared_ptr<PlayerComponent> player = std::dynamic_pointer_cast<PlayerComponent>(players[entity]);
+                if (!player)
+                    continue;
+                std::vector<std::any> values = {};
+                values.push_back((float)position3D->x);
+                values.push_back((float)position3D->y);
+                values.push_back((float)position3D->z);
+                std::shared_ptr<IEvent> event = std::make_shared<AEvent>("UpdateCamera", values);
+                reg.addEvent(event);
+            }
+            if (isColliding)
+                continue;
+            if (entity < canJumps.size()) {
+                std::shared_ptr<CanJumpComponent> canJump = std::dynamic_pointer_cast<CanJumpComponent>(canJumps[entity]);
+                if (!canJump)
+                    continue;
+                canJump->canJump = false;
+            }
             updateHitBox(reg, entity, hitBoxes, objPaths, scales, position3D);
         }
     } catch (std::exception e) {
