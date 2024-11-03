4fef18e755fdf7c2d62139b08e6c53b6e477334b
thomaltarix <thomas.boue@epitech.eu>
2024-11-03 11:18:38 +0100

    feat(Systems): Add the single player to the QuentinTheFarmerGame
    Just merged main and copy paste the single player event handler

--- QuentinTheFarmerGame/Systems/Menus/Buttons/SingleplayerButton/SingleplayerButtonInitSystem.cpp
+++ QuentinTheFarmerGame/Systems/Menus/Buttons/SingleplayerButton/SingleplayerButtonInitSystem.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <json/json.h>
 
+#include "AEvent.hpp"
 #include "AButtonInitSystem.hpp"
 #include "GetGraphicalLibrary.hpp"
 #include "SingleplayerButtonInitSystem.hpp"
@@ -18,6 +19,11 @@ SingleplayerButtonInitSystem::SingleplayerButtonInitSystem() :
 
 static void handleThis(ECS::Registry& reg, int idxPacketEntities)
 {
+    std::vector<std::any> values = {};
+
+    values.push_back(std::string(FIRST_GAME_SCENE));
+    std::shared_ptr<IEvent> eventSinglePlayer = std::make_shared<AEvent>("SinglePlayer", values);
+    reg.addEvent(eventSinglePlayer);
 }
 
 void SingleplayerButtonInitSystem::_initButton(ECS::Registry& reg, int idxPacketEntities)
